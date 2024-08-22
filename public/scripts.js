82199f0397697294862bc4b444d40dc1cc361ab2
Stewart Anderson <anderson.d.stewart@gmail.com>
2024-08-22 09:43:23 -0700

    Initial version handed in for the course. Started blanket styling from scratch. Styling overhaul in progress. Modified some structure in posts.ejs so will need to adapt the rest.

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+})
+
 document.addEventListener("DOMContentLoaded", function () {
     const commentBtn = document.getElementById("commentBtn");
     const form = document.getElementById("commentForm");
